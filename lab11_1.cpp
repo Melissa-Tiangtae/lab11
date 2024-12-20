a710ce0da74b65dd757e9df2661f75dc0760ee2b
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-20 14:43:47 +0700

    lab11_1_completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,27 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+
+    srand(time(0));
+
+   int x = rand()%9;
+   string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+
+
+    for(int i=0;i<3;i++){
+         cin.get();
+    }
+
+    cout << "You will get " << grade[x] <<" in this 261102.";
+
+
+    return 0;
+}
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
